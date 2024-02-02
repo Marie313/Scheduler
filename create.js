ed62408db72ac4df6269d75d8296802e114b283d
Marie Zielke <marie.zielke@telekom.de>
2024-02-02 12:04:57 +0100

    first commit

---
+++ create.js
@@ -0,0 +1,218 @@
+//create.js
+function createForm() {
+
+    const formElements = [
+        { type: 'label', text: 'ID: ' },
+        { type: 'input', attributes: { type: 'text', placeholder: 'ID', class: 'ID'} },
+        { type: 'label', text: 'Name: ' },
+        { type: 'input', attributes: { type: 'text', placeholder: 'enter new name', class:'name' } },
+        { type: 'label', text: 'description: ' },
+        { type: 'input', attributes: { type: 'text', placeholder: 'enter new description', class:'description' } },
+        { type: 'label', text: 'Enabled: ' },
+        { type: 'input', attributes: { type: 'checkbox', class: 'my-checkbox' } },
+        { type: 'p', text: 'false', class: 'message' },
+        { type: 'label', text: 'status: ' },
+        { type: 'input', attributes: { type: 'text', placeholder: 'success', class: 'status' } },
+        { type: 'label', text: 'First Run: ' },
+        { type: 'input', attributes: { type: 'datetime-local', class: 'firstRun'}},
+        { type: 'br' },
+        { type: 'label', text: 'Next Run: ' },
+        { type: 'input', attributes: { type: 'datetime-local', class: 'nextRun'}},
+        { type: 'br' },
+        { type: 'label', text: 'activ until: ' },
+        { type: 'input', attributes: { type: 'datetime-local', class: 'activeuntil'}},
+        { type: 'label', text: 'Interval (in seconds): ' },
+        { type: 'input', attributes: { type: 'text', placeholder: 'enter interval in seconds', class: 'interval'} },
+    ];
+
+    // Container-Element erstellen
+    const container = document.createElement('div');
+    container.classList.add('border'); 
+
+    const heading = document.createElement('u');
+    heading.innerHTML = '<h1>Create</h1>';
+    container.appendChild(heading);
+
+    formElements.forEach(element => {
+        const el = document.createElement(element.type);
+        
+        if (element.text) {
+            el.textContent = element.text;
+        }
+
+        if (element.attributes) {
+            for (const [key, value] of Object.entries(element.attributes)) {
+                el.setAttribute(key, value);
+            }
+        }
+
+        container.appendChild(el);
+    });
+
+    document.body.appendChild(container);
+
+    // Überprüfung des First Run-Datums
+    const firstRunInput = document.querySelector('.firstRun');
+    const nextRunInput = document.querySelector('.nextRun');
+    const activeuntilInput = document.querySelector('.activeuntil');
+    
+    firstRunInput.addEventListener('change', function () {
+        updateInterval();
+        const selectedDateFirst = new Date(firstRunInput.value);
+        const selectedDateNext = new Date(nextRunInput.value);
+        const currentDate = new Date();
+        const maxDate = new Date();
+        const selectedActivUntil = new Date(activeuntilInput.value);
+        maxDate.setFullYear(currentDate.getFullYear() + 1);
+
+        if (selectedDateFirst < currentDate || selectedDateFirst > maxDate || selectedDateFirst >= selectedDateNext || selectedDateFirst > selectedActivUntil) {
+            alert('Das ausgewaehlte Datum fuer "first run" darf weder in der Vergangenheit, noch mehr als ein Jahr in der Zukunft liegen. Des weiteren sind die Faell, dass das Datum fuer "first run" vor dem Datum fuer "next run" oder "active until" liegt, ausgeschlossen.');
+            firstRunInput.value = ''; 
+        }
+    });
+
+    // Überprüfung des Next Run-Datums
+    nextRunInput.addEventListener('change', function () {
+        updateInterval();
+        const selectedDateNext = new Date(nextRunInput.value);
+        const selectedDateFirst = new Date(firstRunInput.value);
+        const selectedActivUntil = new Date(activeuntilInput.value);
+
+        if (selectedDateNext <= selectedDateFirst || selectedDateNext > selectedActivUntil) {
+            alert('Das ausgewaehlte Datum fuer "next run" darf zeitlich nicht vor "first Run" oder "activ until" gelegen sein.');
+            nextRunInput.value = ''; 
+        }
+    });
+
+    activeuntilInput.addEventListener('change', function(){
+        const selectedActivUntil = new Date(activeuntilInput.value);
+        const selectedDateFirst = new Date(firstRunInput.value);
+        const selectedDateNext = new Date(nextRunInput.value);
+
+        if (selectedActivUntil < selectedDateFirst || selectedActivUntil < selectedDateNext){
+            alert('Das ausgewaehlte Datum fuer "active until" darf zeitlich gesehen nicht vor "next run" und erst recht nicht vor "first run" liegen.');
+            activeuntilInput.value = '';
+        }
+    })
+
+    function updateInterval() {
+        const firstRunDate = new Date(firstRunInput.value);
+        const nextRunDate = new Date(nextRunInput.value);
+    
+        if (!isNaN(firstRunDate.getTime()) && !isNaN(nextRunDate.getTime())) {
+            const intervalInSeconds = Math.abs((nextRunDate - firstRunDate) / 1000);
+    
+            const intervalInput = document.querySelector('.interval');
+            if (intervalInput) {
+                intervalInput.value = intervalInSeconds;
+            }
+        }
+    }
+    
+    // Überprüfung des Interval-Felds
+    const intervalInput = document.querySelector('.interval');
+    if (intervalInput) {
+        intervalInput.addEventListener('input', function () {
+            updateNextRun(); 
+        });
+    }
+
+    // Funktion zum Aktualisieren des Next Runs basierend auf dem Interval
+    function updateNextRun() {
+        const firstRunDate = new Date(document.querySelector('.firstRun').value);
+        const intervalInSeconds = parseFloat(intervalInput.value);
+
+        if (!isNaN(firstRunDate.getTime()) && !isNaN(intervalInSeconds)) {
+            const nextRunDate = new Date(firstRunDate.getTime() + intervalInSeconds * 1000 + 3600000);
+
+            const nextRunInput = document.querySelector('.nextRun');
+            if (nextRunInput) {
+                nextRunInput.value = nextRunDate.toISOString().slice(0, 16); // Formatierung für datetime-local
+            }
+    }
+}
+
+    // Button zum Speichern
+    const saveButtonContainer = document.createElement('div');
+    saveButtonContainer.classList.add('save');
+    saveButtonContainer.innerHTML = '<button  onclick="saveElements()">Save</button>';
+    container.appendChild(saveButtonContainer);
+    
+    // Button zum Zurückkehren zum Scheduler
+    const backButtonContainer = document.createElement('div');
+    saveButtonContainer.classList.add('back');
+    backButtonContainer.innerHTML = '<button onclick="redirectToScheduler()">Go back to scheduler</button>';
+    container.appendChild(backButtonContainer);
+
+    document.body.appendChild(container);
+
+    // Checkbox-Logik
+    var checkboxes = document.querySelectorAll(".my-checkbox");
+
+    checkboxes.forEach(function (checkbox) {
+        checkbox.addEventListener('change', function () {
+            const statusElement = checkbox.nextElementSibling; // Nächstes Element nach der Checkbox
+            statusElement.textContent = checkbox.checked ? 'true' : 'false';
+        });
+    });
+}
+
+
+
+// Funktion zum Umleiten zur Index-Seite
+function redirectToScheduler() {
+    window.location.href = 'index.html';
+}
+
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const apiUrl = 'http://20.166.67.112:82/jobs/create';
+ 
+
+async function saveElements() {
+    // Daten sammeln
+    const IDinput = document.querySelector('.ID');
+    const nameInput = document.querySelector('.name');
+    const descriptioninput = document.querySelector('.description');
+    const enabledCheckbox = document.querySelector('.my-checkbox');
+    const statusinput = document.querySelector('.status');
+    const lastRunInput = document.querySelector('.firstRun');
+    const firstRunInput = document.querySelector('.firstRun');
+    const nextRunInput = document.querySelector('.nextRun');
+    const activeUntil = document.querySelector('.activeuntil');
+
+    //nur zur Überprüfung ob daten im richtigen Format vorliegen
+    const firstRunDate = new Date(firstRunInput.value);
+    console.log('First Run:', firstRunDate);
+
+    // Daten für den POST-Request vorbereiten
+    const requestData = {
+        id: IDinput.value,
+        name: nameInput.value,
+        description: descriptioninput.value,
+        enabled: enabledCheckbox.checked,
+        status: statusinput.value,
+        lastRun: new Date(lastRunInput.value).toISOString(),
+        nextRun: new Date(nextRunInput.value).toISOString(),
+        activeFrom: new Date(firstRunInput.value).toISOString(),
+        activeUntil: new Date(activeUntil.value).toISOString(),
+        schedule: "schedule",
+    };
+
+    console.log('Request Data:', JSON.stringify(requestData));
+
+    {
+        // POST-Request an den Server senden
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*', 
+            },
+            body: JSON.stringify(requestData),
+        });
+
+        const newJob = await response.json();
+        console.log('Neuer Job:', newJob);
+    } 
+}
+createForm();
---
+++ index.js
@@ -0,0 +1,188 @@
+const urlParams = new URLSearchParams(window.location.search);
+const selectedVariableId = urlParams.get('id');
+
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const apiUrl = 'http://20.166.67.112:82/jobs';
+
+async function getJobs() {
+    const response = await fetch(apiUrl, {
+        method: 'GET',
+        headers:{
+            'Access-Control-Allow-Origin':'*'
+        }
+    });
+
+    const jobs = await response.json();
+    console.log(jobs);
+    displayJobs(jobs);
+}
+function updateInterval(lastRun, nextRun) {
+    const lastRunDate = new Date(lastRun);
+    const nextRunDate = new Date(nextRun);
+
+    if (!isNaN(lastRunDate.getTime()) && !isNaN(nextRunDate.getTime())) {
+        const intervalInSeconds = Math.abs((nextRunDate - lastRunDate) / 1000);
+        return `${intervalInSeconds} seconds`;
+    } 
+    else{ 
+        return 'not available'
+    }
+}
+
+function displayJobs(jobs) {
+    const jobDetailsTableBody = document.getElementById('jobTable');
+
+    jobs.forEach(job => {
+        const jobDetailRow = document.createElement('tr');
+        jobDetailRow.className = 'job-detail';
+
+    const jobDetailCells = [
+        { value: job.id },
+        { value: job.name },
+        {
+            value: `
+                <div>
+                    <p class="message">${job.enabled ? 'true' : 'false'}</p>
+                </div>
+            `,
+        },
+        { value: job.status },
+        { value: job.lastRun ? formatDate(job.lastRun) : 'not available'},
+        { value: job.nextRun ? formatDate(job.nextRun) : 'not available'},
+        { value: updateInterval(job.lastRun,job.nextRun)},
+        {
+            value: `
+                <div class="edit"><a href="edit.html?id=${job.identification}"><svg width="16" height="16"><use xlink:href="#edit-icon"></use></svg></a></div>
+                <div class="delete"><button onClick="alert('really want to delete this process?')" class="deleteButton"><svg width="16" height="16"><use xlink:href="#delete-icon"></use></svg></button></div>
+            `,
+        },
+    ];
+
+    jobDetailCells.forEach(cell => {
+        const tdElement = document.createElement('td');
+        tdElement.className = cell.className || '';
+        tdElement.innerHTML = cell.value;
+        jobDetailRow.appendChild(tdElement);
+    });
+
+    jobDetailRow.style.backgroundColor = getBackgroundColor(job.status);
+
+    jobDetailsTableBody.appendChild(jobDetailRow);
+});
+
+function formatDate(dateString) {
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+}
+
+
+
+function getBackgroundColor(status) {
+    switch (status) {
+        case 'SUCCESS':
+            return 'rgba(61, 255, 61, 0.75)';
+        case 'WARNING':
+            return 'rgba(255, 252, 71, 0.8)';
+        case 'FAILED':
+            return 'rgba(255, 66, 66, 0.73)';
+        default:
+            return '';
+    }
+}
+
+async function deleteJob(jobId) {
+    try {
+        const response = await fetch(`${apiUrl}/${jobId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to delete job');
+        }
+
+        // Entfernen Sie die Zeile aus der Tabelle, wenn die Anfrage erfolgreich war
+        const rowToRemove = document.getElementById(`job-${jobId}`);
+        rowToRemove.remove();
+    } catch (error) {
+        console.error('Error deleting job:', error.message);
+    }
+}
+
+// Fügen Sie einen Event-Listener hinzu, um auf Klicks auf Lösch-Buttons zu reagieren
+var deleteButtons = document.querySelectorAll('.deleteButton');
+deleteButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        var jobId = button.dataset.jobId; // Nehmen Sie die Job-ID aus dem Dataset
+        if (confirm('Wollen Sie diesen Prozess wirklich loeschen?')) {
+            deleteJob(jobId);
+        }
+    });
+});
+}
+getJobs();
+
+// Event-Listener für die Selectbox 
+var filterSelect = document.getElementById("selectbox");
+filterSelect.addEventListener("change", function () {
+    filterTable(filterSelect.value);
+});
+
+// Event-Listener für das Suchfeld 
+var searchInput = document.getElementById("searchInput");
+searchInput.addEventListener("input", function () {
+    filterTable(filterSelect.value, searchInput.value);
+});
+
+// Funktion zum Filtern der Tabelle basierend auf dem ausgewählten Wert der Selectbox und dem Suchbegriff
+function filterTable(selectedStatus, searchTerm) {
+    var rows = document.querySelectorAll('.job-detail');
+
+    rows.forEach(function (row) {
+        var enabledCell = row.querySelector('.my-checkbox');
+        var isEnabled = enabledCell.checked;
+        var backgroundColor = getComputedStyle(row).backgroundColor;
+
+        // Bedingungen für Vergleich von Hintergrundfarben
+        var colorMatches =
+            (selectedStatus === "all") ||
+            (isEnabled && selectedStatus === "enabled") ||
+            (!isEnabled && selectedStatus === "disabled") ||
+            (selectedStatus === "success" && (
+                backgroundColor === "rgba(61, 255, 61, 0.75)" || backgroundColor === "rgb(61, 255, 61)"
+            )) ||
+            (selectedStatus === "failed" && (
+                backgroundColor === "rgba(255, 66, 66, 0.73)" || backgroundColor === "rgb(255, 66, 66)"
+            )) ||
+            (selectedStatus === "warning" && (
+                backgroundColor === "rgba(255, 252, 71, 0.8)" || backgroundColor === "rgb(255, 252, 71)"
+            ));
+
+        // Bedingungen für die Suche
+        var searchMatches = (
+            !searchTerm || row.textContent.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+
+        // Entscheidung, ob die Zeile angezeigt oder versteckt werden soll
+        if (colorMatches && searchMatches) {
+            row.style.display = "";
+        } else {
+            row.style.display = "none";
+        }
+    });
+}
+document.addEventListener('DOMContentLoaded', function () {
+    new Tablesort(document.getElementById('myTable'));
+});
+
+//Funktion um zum Scheduler zurückkzukehren
+function redirectToEditPage() {
+    window.location.href = `edit.html?id=0`;
+}
+
+//Funktion um neue Seite zu speichern
+function createPage() {
+    window.location.href = `create.html`;
+}
